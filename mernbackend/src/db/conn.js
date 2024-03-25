91d8e57325b81a9e9027b3358ee1c6e350f622be
Rishita77 <rishita.m8@gmail.com>
2024-03-26 01:26:21 +0530

    yayyayay

--- mernbackend/src/db/conn.js
+++ mernbackend/src/db/conn.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose")
 
 mongoose.connect("mongodb://localhost:27017/Blaze", {
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    useCreateIndex:true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }).then(() => {
-    console.log(`connection successful`);
-}).catch((e) =>{
-    console.log(`no connection`);
-})
+    console.log(`Connection successful`);
+}).catch((e) => {
+    console.log(`No connection: ${e}`);
+});

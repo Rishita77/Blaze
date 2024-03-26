2a0794b0a6a2ff747324c774aa356b36cc51988e
Rishita77 <rishita.m8@gmail.com>
2024-03-26 12:17:09 +0530

    hbs install

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -5,7 +5,6 @@ const PORT = process.env.PORT || 3000;
 require("./db/conn");
 const static_path = path.join(__dirname, "../public");
 
-
 app.use(express.static(static_path));
 
 app.set("view engine", "hbs");

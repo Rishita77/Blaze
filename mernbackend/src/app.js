aab2628f7b49e861dd62e401830807b2cee839b6
Rishita77 <rishita.m8@gmail.com>
2024-03-26 12:28:41 +0530

    navbar addition/hbs complete

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -2,15 +2,24 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
+const hbs = require("hbs");
+
 require("./db/conn");
+
 const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
 
 app.use(express.static(static_path));
-
 app.set("view engine", "hbs");
+
+app.set("views", template_path);
+hbs.registerPartials(partials_path)
 app.get("/", (req,res) =>{
     res.render("index")
 });
+
+
 app.listen(PORT, () => {
     console.log(`Server is running at port number ${PORT}`);
 })

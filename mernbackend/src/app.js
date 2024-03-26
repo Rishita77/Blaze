6893b21453106471368bef8f9160af6889b78dd6
Rishita77 <rishita.m8@gmail.com>
2024-03-26 12:06:59 +0530

    html connect

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -7,8 +7,10 @@ const static_path = path.join(__dirname, "../public");
 
 
 app.use(express.static(static_path));
+
+app.set("view engine", "hbs");
 app.get("/", (req,res) =>{
-    res.send("ScoobyDoo!")
+    res.render("index")
 });
 app.listen(PORT, () => {
     console.log(`Server is running at port number ${PORT}`);

1885c52f2370ffd3640082fd4f2b52b201279769
Rishita77 <rishita.m8@gmail.com>
2024-03-28 17:20:50 +0530

    backy

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+require("./db/conn");
+
 const port = process.env.port || 3000;
+
+const static_path = path.join(__dirname, "../public");
+
+app.use(express.static(static_path))
 
 
 app.get("/", (req, res) => {
--- mernbackend/src/db/conn.js
+++ mernbackend/src/db/conn.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose")
 
 mongoose.connect("mongodb://localhost:27017/Blaze", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    // useCreateIndex:true
 }).then(() => {
     console.log(`Connection successful`);
 }).catch((e) => {

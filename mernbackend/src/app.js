7f6688788236c2b86d7a6330da6cd8fe1757f5d2
Rishita77 <rishita.m8@gmail.com>
2024-03-28 17:53:45 +0530

    backend update

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const hbs = require("hbs")
 require("./db/conn");
 
 const port = process.env.port || 3000;
 
 const static_path = path.join(__dirname, "../public");
+const partials_path = path.join(__dirname, "../partials");
 
-app.use(express.static(static_path))
+app.use(express.static(static_path));
+app.set("view engine", "hbs");
+hbs.registerPartials(partials_path);
 
 
 app.get("/", (req, res) => {
-    res.render("ScoobyDoo!");
+    res.render("index");
+});
+
+app.get("/register", (req,res) => {
+    res.render("register");
 });
 
 app.listen(port, () => {
